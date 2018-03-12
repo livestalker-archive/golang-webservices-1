8726abd3e2f8837ee5d2dac94330cd74e80039ef
Alexey Grebenshchikov <mi.aleksio@gmail.com>
2018-03-12 17:17:16 +0300

    add stubs

--- hw2_signer/signer.go
+++ hw2_signer/signer.go
@@ -1,3 +1,23 @@
 package main
 
-// сюда писать код
+import (
+	"fmt"
+)
+
+func main() {
+}
+
+func ExecutePipeline(jobs ...job) {
+	for ix, f := range jobs {
+		fmt.Println(ix, f)
+	}
+}
+
+func SingleHash(in, out chan interface{}) {
+}
+
+func MultiHash(in, out chan interface{}) {
+}
+
+func CombineResults(in, out chan interface{}) {
+}
