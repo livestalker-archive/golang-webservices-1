a3f0f8532e95ec46d209719046413605e7dec9bb
Alexey Grebenshchikov <mi.aleksio@gmail.com>
2018-03-29 15:03:53 +0300

    remove some fields

--- hw3_bench/fast.go
+++ hw3_bench/fast.go
@@ -21,13 +21,13 @@ var (
 )
 
 type User struct {
-	Browsers []string
-	Company  string
-	Country  string
-	Email    string
-	Job      string
-	Name     string
-	Phone    string
+	Browsers []string `json:"browsers"`
+	Company  string   `json:"company"`
+	Country  string   `json:"country"`
+	Email    string   `json:"email"`
+	Job      string   `json:"job"`
+	Name     string   `json:"name"`
+	Phone    string   `json:"phone"`
 }
 
 func main() {
