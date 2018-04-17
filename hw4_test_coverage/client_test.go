b81be85b8e761866ae6aafaf7825471a3fa86b10
Alexey Grebenshchikov <mi.aleksio@gmail.com>
2018-04-17 10:16:34 +0300

    new tests

--- hw4_test_coverage/client_test.go
+++ hw4_test_coverage/client_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 )
 
@@ -11,9 +13,26 @@ type TestCase struct {
 	ErrInfo string
 }
 
+const (
+	ACCESS_TOKEN = "1234567890"
+)
+
 func SearchServer(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("hello"))
+	// Get access token
+	accessToken := r.Header.Get("AccessToken")
+	if accessToken != ACCESS_TOKEN {
+		w.WriteHeader(http.StatusUnauthorized)
+	}
+	// Get query prameters
+	limit := r.URL.Query().Get("limit")
+	offset := r.URL.Query().Get("offset")
+	query := r.URL.Query().Get("query")
+	order_field := r.URL.Query().Get("order_field")
+	order_by := r.URL.Query().Get("order_by")
+	log.Printf("limit: %s, offset: %s, query: %s, order_field: %s, order_by: %s", limit, offset, query, order_field, order_by)
+	if v, _ := strconv.Atoi(limit); v >= 999999 {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
 }
 
 // Check imput guards
@@ -23,29 +42,66 @@ func TestFindUserInput(t *testing.T) {
 			Request: SearchRequest{
 				Limit: -1,
 			},
-			ErrInfo: "Limit not checked",
 		},
 		TestCase{
 			Request: SearchRequest{
 				Offset: -1,
 			},
-			ErrInfo: "Offset not checked",
 		},
 	}
 	searchClient := &SearchClient{}
 	for _, item := range cases {
 		_, err := searchClient.FindUsers(item.Request)
 		if err == nil {
-			t.Error(item.ErrInfo)
+			t.Errorf("Input parameters not checked: %#v", item.Request)
 		}
 	}
 }
 
-func TestFindUser(t *testing.T) {
+func TestFindUserAuth(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
 	defer ts.Close()
+	req := SearchRequest{}
 	searchClient := &SearchClient{
-		AccessToken: "1234567890",
+		AccessToken: "wrong access token",
 		URL:         ts.URL,
 	}
+	_, err := searchClient.FindUsers(req)
+	if err == nil {
+		t.Error("AccessToken not checked")
+	}
 }
+
+func TestFindUserInternalError(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+	defer ts.Close()
+	req := SearchRequest{Limit: 999999}
+	searchClient := &SearchClient{
+		AccessToken: ACCESS_TOKEN,
+		URL:         ts.URL,
+	}
+	_, err := searchClient.FindUsers(req)
+	if err == nil {
+		t.Error("Not generate InternalError")
+	}
+}
+
+//func TestFindUser(t *testing.T) {
+//	cases := []TestCase{
+//		TestCase{
+//			Request: SearchRequest{},
+//		},
+//	}
+//	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+//	defer ts.Close()
+//	searchClient := &SearchClient{
+//		AccessToken: "1234567890",
+//		URL:         ts.URL,
+//	}
+//	for _, item := range cases {
+//		_, err := searchClient.FindUsers(item.Request)
+//		if err != nil {
+//			t.Errorf("Error: %s", err)
+//		}
+//	}
+//}
